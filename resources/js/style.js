036785512886c4fad24c595a1b68f5b151954805
Bernardus Billy Tjiptoning <bernardusbilly@Bernarduss-MacBook-Pro.local>
2015-07-17 08:13:57 -0700

    Major Update
    - added chapter 3
    - make a new feature for chapter menu

--- resources/js/style.js
+++ resources/js/style.js
@@ -1,11 +1,16 @@
 $(window).ready(function() {
 	var hideChapter = true;
-	$(".chapter-header").click(function() {
+	$(".chapter-menu").click(function() {
+
 		if (hideChapter == true) {
-			$(".chapter-number").css("display","block");
+			$(".chapter-menu").css("transform", "rotateX(180deg)");
+			$(".chapter-menu").css("margin-bottom", "0");
+			$(".chapter-number").css("transform","translateY(0px)");
 			hideChapter = false;
 		} else {
-			$(".chapter-number").css("display","none");
+			$(".chapter-menu").css("transform", "rotateX(0deg)");
+			$(".chapter-menu").css("margin-bottom", "-35px");
+			$(".chapter-number").css("transform","translateY(30px)");
 			hideChapter = true;
 		}
 	});
